654f138ab946296b4efc43de0952d13e68a4371a
Muhammad Talha Dar <talha.dar@invozone.com>
2022-06-23 15:40:52 +0500

    connection with InfuraAPI and setup signet

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,37 @@
-console.log('Try npm run lint/fix!');
+import {ethers} from 'ethers';
+// eslint-disable-next-line prettier/prettier
+import * as dotenv from 'dotenv';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+dotenv.config();
+async function main() {
+  const NETWORK = process.env.ETHEREUM_NETWORK;
+  const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+  const WETH_CONTRACT_ADDRESS = process.env.WETH_CONTRACT_ADDRESS;
+  const SIGNER = process.env.SIGNER_PRIVATE_KEY;
+  const rpcUri = `https://${NETWORK}.infura.io/v3/${INFURA_PROJECT_ID}`;
 
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
+  if (SIGNER === undefined) {
+    throw Error('SIGNER is underfined');
   }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUri);
+  const providerSigner = new ethers.Wallet(SIGNER, provider);
+
+  console.log(await providerSigner.getAddress());
+  // const filter = {
+  //   address: WETH_CONTRACT_ADDRESS,
+  //   topics: [
+  //     // the name of the event, parnetheses containing the data type of each event, no spaces
+  //     ethers.utils.id('Transfer(address,address,uint256)'),
+  //   ],
+  // };
+  // provider.on(filter, async () => {
+  //   console.log(await provider.getBlockNumber());
+  // });
 }
-// TODO: more examples
+
+main()
+  .then(() => {
+    console.log('main()...');
+  })
+  .catch(console.error);
